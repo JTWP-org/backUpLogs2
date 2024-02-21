d3760d1dc24618f33ab7a3a6341105688cf7f6a6
Salted Cracker Jack <87721660+oneSALTycracker@users.noreply.github.com>
2024-02-20 19:26:29 -0500

    Update bkUp.sh
    added a fix to maintain original path inside logs backup

--- bkUp.sh
+++ bkUp.sh
@@ -3,64 +3,60 @@
 ##################################
 
 backUpDir="/home/steam/logs"
-
-whereIsThisScript=$(pwd)
-
+whereIsThisScript="/home/steam/backUpLogs2"
 webHook="true"
 
-    #works best if u use a discord url 
+# works best if u use a discord url
 icon='https://cdn.discordapp.com/attachments/932110907729920080/1130204308315578459/6328646.png'
 
-    #finding all servers installed 
+# finding all servers installed
 servers=( $(ls /home/steam/*/PavlovServer.sh | awk -F "\/" '{print "/" $2 "/" $3 "/" $4}' ) )
 
-
-
-
-
-    #loading webhook variable from .WEBHOOK FILE 
+# loading webhook variable from .WEBHOOK FILE
 source .WEBHOOK
 
-    #update server
+# update server
 sudo apt-get update
 
-echo """  __ServerStuff__ 
+echo """  __ServerStuff__
     Uptime: $(uptime -p)
     server updated """ | tee hook
-    echo '..............' | tee -a hook 
+echo '..............' | tee -a hook
 
-
-    #loop for data formating and colection 
+# loop for data formatting and collection
 for server in "${servers[@]}"
 do
-        #build files if missing 
-    serverFldr=$(echo  ${server} | cut -d/ -f 4-  ) 
+    # build files if missing
+    serverFldr=$(echo ${server} | cut -d/ -f 4-  )
     mkdir -p "${backUpDir}/${serverFldr}/Pavlov/Saved/Logs/"
 
-        #gets name for server from Game.ini
+    # gets name for server from Game.ini
     name=$( cat "${server}/Pavlov/Saved/Config/LinuxServer/Game.ini" | grep 'Name' | awk -F "=" '{print $2}' )
-    
-     printf '%s\n' """ Starting backup for logs on server: 
-      **${name}** 
-       *$(date +%x" "%r)*  
-     """ | tee -a hook 
 
+    printf '%s\n' """ Starting backup for logs on server:
+      **${name}**
+       *$(date +%x" "%r)*
+     """ | tee -a hook
 
     echo ""
-    printf '%s\n' " \`logs to copy: $(ls ${server}/Pavlov/Saved/Logs/ | grep Pavlov-backup-* )\` " | tee -a hook 
-        #gets all the current log backups in each server dir then move them to dest dir you can change it below 
-    ls ${server}/Pavlov/Saved/Logs/ | grep Pavlov-backup* | xargs -I %  mv "${server}/Pavlov/Saved/Logs/%" "${backUpDir}$(echo ${server} | cut -d/ -f 7-)" | tee -a hook 
-    echo "Finished with   - ${name}" | tee -a hook 
-    echo '..............' | tee -a hook 
+    printf '%s\n' " \`logs to copy: $(find ${server}/Pavlov/Saved/Logs/ -type f -name 'Pavlov-backup-*.log')\` " | tee -a hook
+    
+    # Find and move the log files
+    find "${server}/Pavlov/Saved/Logs/" -type f -name 'Pavlov-backup-*.log' -exec sh -c '
+        for file; do
+            dest="${backUpDir}$(echo ${server} | cut -d/ -f 4-)/$(dirname "${file#${server}/Pavlov/Saved/Logs/}")"
+            mkdir -p "$dest"
+            mv "$file" "$dest"
+        done
+    ' sh {} +
+
+    echo "Finished with - ${name}" | tee -a hook
+    echo '..............' | tee -a hook
 done
 
-
-
 if [ "$webHook" = "true" ]; then
-        #WEBHOOK code sourced from here https://github.com/fieu/discord.sh
-    bash "${whereIsThisScript}/discord.sh" --webhook-url "$webhook" --title "SERVER LOG BACKUP BOT \n Bot by JTWP" --thumbnail "${icon} " --text "this script can be found at [GITHUB](https://github.com/JTWP-org/backUpLogs2) " --url "http://JTWP.org" --description "$(jq -Rs . <hook | cut -c 2- | rev | cut -c 2- | rev)"   | tee -a hook  
+    # WEBHOOK code sourced from here https://github.com/fieu/discord.sh
+    bash "${whereIsThisScript}/discord.sh" --webhook-url "$webhook" --title "SERVER LOG BACKUP BOT \n Bot by JTWP" --thumbnail "${icon} " --text "this script can be found at [GITHUB](https://github.com/JTWP-org/backUpLogs2) " --url "http://JTWP.org" --descrip>fi
 fi
-
-
 
 echo "" > hook
